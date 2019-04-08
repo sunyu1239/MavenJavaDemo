be5f84c7eb3b41b04c6884dd1bb9dda153c3299e
sunyu1239 <sunyu1239@yahoo.com>
2019-04-08 17:09:11 +0800

    hhh

---
+++ src/main/java/day0408/DBCPPool.java
@@ -0,0 +1,82 @@
+package day0408;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class DBCPPool {
+    public static void main(String[] args) {
+        System.out.println("Setting up data source.");
+        String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+        DataSource dataSource = setupDataSource(url);
+        System.out.println("Done...");
+
+        // Now, we can use JDBC DataSource as we normally would.
+        //
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rset = null;
+
+        try {
+            System.out.println("Creating connection.");
+            conn = dataSource.getConnection();
+            System.out.println("adress:" + conn);
+            System.out.println("Creating statement.");
+            stmt = conn.createStatement();
+            System.out.println("Executing statement.");
+            rset = stmt.executeQuery("select * from student");
+            System.out.println("Results:");
+            int numcols = rset.getMetaData().getColumnCount();
+            while (rset.next()) {
+                for (int i = 1; i <= numcols; i++) {
+                    System.out.print("\t" + rset.getString(i));
+                }
+                System.out.println("");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rset != null)
+                    rset.close();
+            } catch (Exception e) {
+            }
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (Exception e) {
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    public static DataSource setupDataSource(String connectURI) {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        ds.setUsername("root");
+        ds.setPassword("root");
+        ds.setUrl(connectURI);
+        return ds;
+    }
+
+    public static void printDataSourceStats(DataSource ds) {
+        BasicDataSource bds = (BasicDataSource) ds;
+        System.out.println("NumActive: " + bds.getNumActive());
+        System.out.println("NumIdle: " + bds.getNumIdle());
+    }
+
+    public static void shutdownDataSource(DataSource ds) throws SQLException {
+        BasicDataSource bds = (BasicDataSource) ds;
+        bds.close();
+    }
+
+}
